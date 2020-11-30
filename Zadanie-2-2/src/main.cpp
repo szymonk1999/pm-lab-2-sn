5efcc5269492fa67e723648eb23fc17d41946ac6
„Szymon <szymonk@onet.pl>
2020-11-30 09:18:52 +0100

    Zadanie  2-2-1

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+int main()
+{
+  uint32_t i;
+  DDRB |= (1 << 5);
+  while (1)
+  {
+    PORTB |= (1 <<5);
+    i = 0x3FFF;
+    do
+    {
+      __asm__ __volatile__("nop");
+    } while (i--);
+    PORTB &= !(1 << 5);
+    i = 0x3FFF;
+    do
+    {
+      _asm__ __volatile__("nop");
+    } while (i--);
+    
+    
+  }
 }
