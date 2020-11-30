724ed78c37e1247b94c6f700edaac89cb402d638
„Szymon <szymonk@onet.pl>
2020-11-30 09:41:19 +0100

    Wykonano  zadanie 2.2.3–wskaźniki

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,24 +1,38 @@
 #include <avr/io.h>
-#define LED_LENGTH 8 
+
+uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
+uint8_t *pLedState = ledState;
+bool state = true;
+
+void togglePinD13(bool state)
+{
+  PORTB = (state << 5);
+  state = !(state);
+}
+
+void delay()
+{
+  for (uint32_t j = 0x2FFFF; j > 0; j--){
+    asm volatile("nop");
+  }
+}
 
 int main()
 {
-  DDRD |= 0xFF;
-  while (1)
-  {
-    for (uint8_t i = 0; i < LED_LENGTH; i++)
-    {
-      PORTD = (1<<i);
-      for (uint32_t j=0x1FFFF; j > 0; j--){
-        __asm__ __volatile__("nop");
-      }
+  DDRB |=(1 << 5);
+  DDRD |=0xFF;
+  while (1){
+    togglePinD13(&state);
+    delay();
+    for (uint8_t i = 0; i < sizeof(ledState); i++){
+      PORTD = ledState[i];
+      delay();
     }
-    for (uint8_t i = 1; i < (LED_LENGTH - 1); i++){
-      PORTD = (PORTD >> 1);
-      for (uint32_t j = 0x1FFFF; j > 0; j--)
-      {
-       __asm__ __volatile__("nop");
-      }
+    pLedState = ledState;
+    for (uint8_t i = 0; i < sizeof(ledState); i++){
+      PORTD = *pLedState;
+      pLedState++;
+      delay();
     }
   }
 }
