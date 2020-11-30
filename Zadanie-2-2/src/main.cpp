1a52f4129c581b67b1a84a0df2c4d54b68ffced9
„Szymon <szymonk@onet.pl>
2020-11-30 22:55:55 +0100

    Wykonano   zadanie dodatkowe 3.2

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,39 +1,56 @@
-#include <avr/io.h>
-#define SOLUTION 0
-uint8_t button;
-bool state = true;
+#include <SoftwareSerial.h>
+const int buttonPin1 = 10;     
+const int buttonPin2 =  11;      
 
-void togglePinD13(bool *state){
-  PORTB = (*state << 5);
-  *state = !(*state);
-}
+int diody[] = {2, 3, 4, 5, 6, 7, 8, 9};
+#define SIZE 8
+int i;
+int s = 1;
+int buttonState1 = 0;
+int buttonState2 = 0;  
 
-void delay() {
-  for (uint32_t j = 0x1FFFF; j > 0; j--)
-  asm volatile ("nop");
-}
-int main(){
-  DDRB &=!(1 << 0);
-  DDRB |= (1 << 5);
-  while (1)
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+
+  for (i = 0; i < SIZE; i++)
   {
-    #if (SOLUTION == 0)
-    button = (PINB & (1 << PINB0));
-    if (button == 0){
-      togglePinD13(&state);
-      delay();
-    }
-    else
-    delay();
-    #elif (SOLUTION == 1)
-    while (!(PINB & (1 << PINB0))){
-      togglePinD13(&state);
-      delay();
-
-    }
-    #endif
-
-
-
+    pinMode(diody[i], OUTPUT);
   }
+}
+
+void loop() {
+  
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+
+if (buttonState1 == HIGH) {
+s = 1;
+}
+
+if (buttonState2 == HIGH) {
+s = 2;
+}
+
+Serial.println(s);
+  
+if (s == 2){
+for (i = 0; i < SIZE; i++) 
+{
+    digitalWrite(diody[i], HIGH);
+    delay(200);
+    digitalWrite(diody[i], LOW);
+  }
+}
+  
+  if (s == 1){
+for (i = 8; i > 0; i--) 
+{
+    digitalWrite(diody[i], HIGH);
+    delay(200);
+    digitalWrite(diody[i], LOW);
+  }
+}
+
 }
